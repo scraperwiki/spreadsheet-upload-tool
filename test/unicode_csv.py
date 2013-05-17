c79edc7a9628250db5d90055ec738031c5c16106
Zarino Zappia <mail@zarino.co.uk>
2013-05-17 12:39:59 +0100

    handle unicode CSVs better

--- test/unicode_csv.py
+++ test/unicode_csv.py
@@ -13,12 +13,22 @@ def test_it_detects_unicode_csv_file():
     assert_equals(filetype, 'csv')
 
 
-def test_it_can_extract_a_simple_xls_file():
+def test_it_can_extract_a_unicode_csv():
     sheets = extract.extract('fixture/mps_unicode.csv')
     assert_equals(len(sheets), 1)
 
     sheet = sheets['swdata']
     assert_equals(len(sheet), 653)
     row = sheet[460]
+    assert_equals(row['MP Name'], 'Michelle Gildernew')
+    assert_equals(row['Party'], u'Sinn Féin')
+    
+
+def test_it_saves_a_unicode_csv_to_the_database():
+    sheets = extract.extract('fixture/mps_unicode.csv')
+    extract.save(sheets)
+
+    data = scraperwiki.sql.select('* from swdata')
+    row = data[460]
     assert_equals(row['MP Name'], 'Michelle Gildernew')
     assert_equals(row['Party'], u'Sinn Féin')
